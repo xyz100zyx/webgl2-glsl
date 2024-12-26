a84d64dca70458a48d1f276937ad2be986bbf463
artyom.karpinchick <artsiom.karpinchyk@indev.by>
2024-12-26 14:34:23 +0300

    vertex attributes changes data array buffer

--- main.js
+++ main.js
@@ -74,41 +74,62 @@ precision mediump float;
   gl.enableVertexAttribArray(ATTRIBUTE_LOCATION_position);
   gl.enableVertexAttribArray(ATTRIBUTE_LOCATION_color);
 
-  const bufferData = new Float32Array([
-    0, 1,       100,        1,0,0, 
-    -1, -1,     32,         0,1,0,
-    1, -1,      50,         0,0,1,
-  ]);
+  // block of the buffer for pisitions
+  /* 
+    [
+    0, 1, => first point position
+    -1, -1, => second point position
+    1, -1 => thrid point position
+    ]
+  */
+  const positionBufferData = new Float32Array([0, 1, -1, -1, 1, -1]);
+  const positionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, positionBufferData, gl.STATIC_DRAW);
 
-  const buffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-  gl.bufferData(gl.ARRAY_BUFFER, bufferData, gl.STATIC_DRAW);
+  // block of the buffer for point size
+  /* 
+    [
+    100, => first point size
+    32, => second point size
+    50 => thrid point size
+    ]
+  */
+  const pointSizeBufferData = new Float32Array([100, 32, 50]);
+  const pointSizeBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, pointSizeBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, pointSizeBufferData, gl.STATIC_DRAW);
 
-  gl.vertexAttribPointer(
-    ATTRIBUTE_LOCATION_position,
-    2,
-    gl.FLOAT,
-    false,
-    6 * 4,
-    0 * 4
-  );
+  // block of the buffer for color
+  /* 
+    [
+    1, 0, 0, => red
+    0, 1, 0, => green
+    0, 0, 1 => blue
+    ]
+  */
+  const colorBufferData = new Float32Array([1, 0, 0, 0, 1, 0, 0, 0, 1]);
+  const colorBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, colorBufferData, gl.STATIC_DRAW);
+
+  // block: join the "positionBufferData" buffer data with attribute pointer
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  gl.vertexAttribPointer(ATTRIBUTE_LOCATION_position, 2, gl.FLOAT, false, 0, 0);
+
+  // block: join the "pointSizeBuffer" buffer data with attribute pointer
+  gl.bindBuffer(gl.ARRAY_BUFFER, pointSizeBuffer);
   gl.vertexAttribPointer(
     ATTRIBUTE_LOCATION_pointSize,
     1,
     gl.FLOAT,
     false,
-    6 * 4,
-    2 * 4
+    0,
+    0
   );
 
-  gl.vertexAttribPointer(
-    ATTRIBUTE_LOCATION_color,
-    3,
-    gl.FLOAT,
-    false,
-    6 * 4,
-    3 * 4
-  );
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+  gl.vertexAttribPointer(ATTRIBUTE_LOCATION_color, 3, gl.FLOAT, false, 0, 0);
 
   gl.drawArrays(gl.TRIANGLES, 0, 3);
 })();
